b22420ef3de40b842726117b4f70d8e00c15f401
Jacob Gadberry <jagadberry03@gmail.com>
2026-02-08 20:44:56 -0500

    Add TODO comment plugin for NeoVim

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -59,4 +59,14 @@ return {
 			require("configs.mason-conform")
 		end,
 	},
+	{
+		"folke/todo-comments.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
