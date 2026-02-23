c2fb65ebbf3b6fbd280ac89108ad8b96355554fc
Jacob Gadberry <jacob.gadberry@temenos.com>
2026-02-22 22:25:26 -0500

    Remove tmux function from WezTerm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -19,81 +19,81 @@ config.font = wezterm.font({
 })
 
 -- tmux-like bindings for windows
-config.leader = { key = "q", mods = "CTRL", timeout_millisecond = "2000" }
-config.keys = {
-	{
-		mods = "LEADER",
-		key = "c",
-		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		mods = "LEADER",
-		key = "x",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-	{
-		mods = "LEADER",
-		key = "b",
-		action = wezterm.action.ActivateTabRelative(-1),
-	},
-	{
-		mods = "LEADER",
-		key = "n",
-		action = wezterm.action.ActivateTabRelative(1),
-	},
-	{
-		mods = "LEADER",
-		key = "\\",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "LEADER",
-		key = "-",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		mods = "LEADER",
-		key = "h",
-		action = wezterm.action.ActivatePaneDirection("Left"),
-	},
-	{
-		mods = "LEADER",
-		key = "j",
-		action = wezterm.action.ActivatePaneDirection("Down"),
-	},
-	{
-		mods = "LEADER",
-		key = "k",
-		action = wezterm.action.ActivatePaneDirection("Up"),
-	},
-	{
-		mods = "LEADER",
-		key = "l",
-		action = wezterm.action.ActivatePaneDirection("Right"),
-	},
-	{
-		mods = "LEADER",
-		key = "LeftArrow",
-		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "RightArrow",
-		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "DownArrow",
-		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
-	},
-	{
-		mods = "LEADER",
-		key = "UpArrow",
-		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
-	},
-	{ key = "k", mods = "SHIFT|CTRL", action = wezterm.action.ScrollByPage(-0.5) },
-	{ key = "j", mods = "SHIFT|CTRL", action = wezterm.action.ScrollByPage(0.5) },
-}
+-- config.leader = { key = "q", mods = "CTRL", timeout_millisecond = "2000" }
+-- config.keys = {
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "c",
+-- 		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "x",
+-- 		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "b",
+-- 		action = wezterm.action.ActivateTabRelative(-1),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "n",
+-- 		action = wezterm.action.ActivateTabRelative(1),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "\\",
+-- 		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "-",
+-- 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "h",
+-- 		action = wezterm.action.ActivatePaneDirection("Left"),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "j",
+-- 		action = wezterm.action.ActivatePaneDirection("Down"),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "k",
+-- 		action = wezterm.action.ActivatePaneDirection("Up"),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "l",
+-- 		action = wezterm.action.ActivatePaneDirection("Right"),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "LeftArrow",
+-- 		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "RightArrow",
+-- 		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "DownArrow",
+-- 		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+-- 	},
+-- 	{
+-- 		mods = "LEADER",
+-- 		key = "UpArrow",
+-- 		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+-- 	},
+-- 	{ key = "k", mods = "SHIFT|CTRL", action = wezterm.action.ScrollByPage(-0.5) },
+-- 	{ key = "j", mods = "SHIFT|CTRL", action = wezterm.action.ScrollByPage(0.5) },
+-- }
 
 -- Scrolling behavior
 -- How many lines of scrollback you want to retain per tab
